e8be2bc2ee976e9e1fb8a6ba3f7033fb60d06565
Anna Maria <aannamariabr@outlook.com>
2023-10-31 13:26:25 -0300

    Search route

--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -5,6 +5,8 @@ export async function GET(
   _: Request,
   { params }: { params: { slug: string } },
 ) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
   const slug = z.string().parse(params.slug)
 
   const product = data.products.find((product) => product.slug === slug)
